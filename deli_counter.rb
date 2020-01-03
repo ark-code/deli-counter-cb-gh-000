a446e9294e320c4fed0776ebb2b780ca62e42e25
Akshay K <akshaykunti1@gmail.com>
2020-01-03 11:00:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,16 @@
 # Write your code here.
-def line 
-  puts "The line is currently empty."
+def line (arr)
+  if arr.size > 0 
+    string =  "The line is currently:"
+    counter = 1
+    arr.each {|e| 
+    string = string +" "+counter+"."+" #{e}"
+    counter+=1
+    }
+    puts string
+  else
+    puts "The line is currently empty."
+  end
 end
 
 def take_a_number(arr, str)
